662805177b522444b41bab33df8e17190659c0ff
ArturoMarmolejo <arthur.m.mart@gmail.com>
2023-05-30 12:11:27 -0400

    Successfully implemented search view

--- app/src/main/java/com/arturomarmolejo/amiiboappamcompose/views/screens/HomeScreen.kt
+++ app/src/main/java/com/arturomarmolejo/amiiboappamcompose/views/screens/HomeScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,9 +21,14 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,11 +36,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -42,20 +53,77 @@ import com.arturomarmolejo.amiiboappamcompose.model.domain.AmiiboDomain
 import com.arturomarmolejo.amiiboappamcompose.utils.UIState
 import com.arturomarmolejo.amiiboappamcompose.viewmodel.AmiiboViewModel
 import com.arturomarmolejo.amiiboappamcompose.views.navgraph.Routes
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(amiiboViewModel: AmiiboViewModel, navController: NavController) {
-    when(val state = amiiboViewModel.amiiboList.collectAsState(UIState.LOADING).value) {
-        is UIState.LOADING -> {}
-        is UIState.SUCCESS -> {
-            AmiiboList(state.response, navController) {
-                amiiboViewModel.selectedAmiibo = it
+    var searchQuery by remember {
+        mutableStateOf("")
+    }
+    val filteredItems = remember {
+        derivedStateOf {
+            if (searchQuery.isEmpty()) {
+                amiiboViewModel.amiiboList.value
+            } else {
+                amiiboViewModel.amiiboList.value
             }
         }
-        is UIState.ERROR -> {
-            Toast.makeText(LocalContext.current, "Item not found", Toast.LENGTH_LONG).show()
-        }
     }
+    Column(Modifier.fillMaxSize()) {
+   
+        Text(
+            text = "Amiibo Catalog",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .wrapContentHeight(),
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Search for a specific Amiibo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .wrapContentHeight()
+                .padding(2.dp),
+            fontFamily = FontFamily(Font(R.font.poppins_regular)),
+            color = Color.Black,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //--------SEARCHVIEW HERE ---------------
+        TextField(
+            value = searchQuery, 
+            onValueChange = {searchQuery = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = { Text(text = "Search")},
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        when(val state = amiiboViewModel.amiiboList.collectAsState(UIState.LOADING).value) {
+            is UIState.LOADING -> {}
+            is UIState.SUCCESS -> {
+                FilteredAmiiboList(items = state.response, selectedItem = {
+                    amiiboViewModel.selectedAmiibo = it
+                }, navController = navController, searchQuery = searchQuery)
+            }
+            is UIState.ERROR -> {
+                Toast.makeText(LocalContext.current, "Item not found", Toast.LENGTH_LONG).show()
+            }
+        }
+    
+    }
+
 }
 
 @Composable
@@ -64,22 +132,44 @@ fun AmiiboList(
     navController: NavController? = null,
     selectedItem: ((AmiiboDomain) -> Unit)? = null,
 ) {
+        Column(
+            Modifier
+                .background(color = Color.White)
+                .fillMaxSize()
+        ) {
+            LazyColumn(
+                content = {
+                    itemsIndexed(items = items) { _, item ->
+                        AmiiboItem(item = item, navController, selectedItem )
 
-    Column(
-        Modifier
-            .background(color = Color.White)
-            .fillMaxSize()
-    ) {
-        LazyColumn(
-            content = {
-                itemsIndexed(items = items) { _, item ->
-                    AmiiboItem(item = item, navController, selectedItem )
-                    
+                    }
+                }
+            )
+        }
+}
+
+@Composable
+fun FilteredAmiiboList(
+    items: List<AmiiboDomain>,
+    searchQuery: String,
+    selectedItem: ((AmiiboDomain) -> Unit)? = null,
+    navController: NavController? = null,
+) {
+    val filteredItemsState = remember(searchQuery) {
+        derivedStateOf {
+            if (searchQuery.isEmpty()) {
+                items
+            } else {
+                items.filter {
+                    it.name.contains(searchQuery, ignoreCase = true)
                 }
             }
-        )
+        }
     }
+    
+    val filteredItems = filteredItemsState.value
 
+    AmiiboList(items = filteredItems, navController = navController, selectedItem = selectedItem)
 }
 
 
